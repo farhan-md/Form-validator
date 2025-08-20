295fb74052a1c62c4e1550a41aac88be46afd7cb
farhan-md <f.majidi1924@gmail.com>
2025-08-21 01:42:47 +0330

    Implemented email validation

--- form_validator/validators.py
+++ form_validator/validators.py
@@ -1,6 +1,6 @@
 import re
 
-email_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9]\.[A-Za-z]{2,}$")
+email_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
 
 def validate_form(data):
     errors = {}
@@ -14,3 +14,22 @@ def validate_form(data):
         else:
             data[field] = str(value).strip()
         
+    
+    email = data.get("email","")
+
+    if email and not email_pattern.match(email):
+        errors["email"] = "The email format is not valid"
+    
+    if len(errors) == 0:
+        is_valid = True
+    else:
+        is_valid = False
+
+    return is_valid , errors
+
+form1 = {"name": "Ali", "email": "ali@example.com", "message": "salam"}
+form2 = {"name": "", "email": "wrong@", "message": "  "}
+
+
+print(validate_form(form1))
+print(validate_form(form2))
